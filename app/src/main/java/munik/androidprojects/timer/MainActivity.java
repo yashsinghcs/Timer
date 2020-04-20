81c9febb7fe07177a6ba93aca70bc6d37ad770b6
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-04-20 20:02:27 +0530

    added the conntection between seekbar and counter to xml

--- app/src/main/java/munik/androidprojects/timer/MainActivity.java
+++ app/src/main/java/munik/androidprojects/timer/MainActivity.java
@@ -1,14 +1,21 @@
 package munik.androidprojects.timer;
 
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    SeekBar timerCount;
+    TextView counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timerCount = (SeekBar) findViewById(R.id.timerCount);
+        counter = (TextView) findViewById(R.id.counter);
+
     }
 }
