97a4d35fe46f9f3fb391c5e6dd384a4461456571
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-04-20 20:09:44 +0530

    added code to diplay the amount of timer setted to the counter via seek bar

--- app/src/main/java/munik/androidprojects/timer/MainActivity.java
+++ app/src/main/java/munik/androidprojects/timer/MainActivity.java
@@ -16,6 +16,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         timerCount = (SeekBar) findViewById(R.id.timerCount);
         counter = (TextView) findViewById(R.id.counter);
+        timerCount.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateCounter(progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    private void updateCounter(int counterInSec) {
+        int minutes = counterInSec / 60;
+        int sec = counterInSec - minutes;
+        counter.setText(Integer.toString(minutes) + ":" + Integer.toString(sec));
 
     }
 }
