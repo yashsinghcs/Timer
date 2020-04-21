9719dc43c61ee14d6b466bcaa48afd00b3a2a57b
yashsinghcs <yash.singh_cs18@gla.ac.in>
2020-04-22 02:46:29 +0530

    added constraint tha timer must be greater than 00:00 else toast a msg warning

--- app/src/main/java/munik/androidprojects/timer/MainActivity.java
+++ app/src/main/java/munik/androidprojects/timer/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,32 +19,42 @@ public class MainActivity extends AppCompatActivity {
     CountDownTimer countDownTimer;
 
     public void timer(View view) {
+        playSound = MediaPlayer.create(this, R.raw.rooster_times_up);
+        if (timerCount.getProgress() != 0) {
+            if (startStop.getText().equals("START!")) {
+//            Log.i("place", "start");
+                timerCount.setVisibility(View.INVISIBLE);
+                int counterInSec = timerCount.getProgress();
+                startStop.setText("RESET!");
+                countDownTimer = new CountDownTimer(counterInSec * 1000, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
 
-        if (startStop.getText().equals("START!")) {
-            startStop.setText("RESET!");
-            timerCount.setVisibility(View.INVISIBLE);
-            int counterInSec = timerCount.getProgress();
-            countDownTimer = new CountDownTimer(counterInSec * 1000, 1000) {
-                @Override
-                public void onTick(long millisUntilFinished) {
-                    updateCounter((int) (millisUntilFinished / 1000));
-                }
+                        updateCounter((int) (millisUntilFinished / 1000));
+                    }
 
-                @Override
-                public void onFinish() {
-                    playSound.start();
-                    playSound.setLooping(true);
+                    @Override
+                    public void onFinish() {
+                        startStop.setText("RESET!");
 
-                }
-            }.start();
+//                    Log.i("finish", "finshesfinish");
+                        playSound.start();
+                        playSound.setLooping(true);
+                    }
+                }.start();
 
-        } else if (startStop.getText().equals("RESET!")) {
-            playSound.stop();
-            startStop.setText("START!");
-            timerCount.setVisibility(View.VISIBLE);
-            counter.setText("00:00");
-            timerCount.setProgress(0);
-            countDownTimer.cancel();
+            } else if (startStop.getText().equals("RESET!")) {
+//            Log.i("place", "reste stop");
+                playSound.setLooping(false);
+                playSound.pause();
+                startStop.setText("START!");
+                timerCount.setVisibility(View.VISIBLE);
+                counter.setText("00:00");
+                timerCount.setProgress(0);
+                countDownTimer.cancel();
+            }
+        } else {
+            Toast.makeText(this, "Please set the timer", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -53,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         timerCount = (SeekBar) findViewById(R.id.timerCount);
         counter = (TextView) findViewById(R.id.counter);
-        playSound = MediaPlayer.create(this, R.raw.rooster_times_up);
+
         startStop = (Button) findViewById(R.id.startStop);
         timerCount.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
